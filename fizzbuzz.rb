aea4b71aeabc26bc393ea564e811bf4acd60f5c9
staticWagomU <to6wa17@gmail.com>
2024-06-06 21:53:05 +0900

    update

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 def fizz_buzz(num)
-  if num % 15 == 0
+  if (num % 15).zero?
     'Fizz Buzz'
-  elsif num % 3 == 0
+  elsif (num % 3).zero?
     'Fizz'
-  elsif num % 5 == 0
+  elsif (num % 5).zero?
     'Buzz'
   else
     num.to_s
