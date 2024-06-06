5f10083d06a5a633e298d4ec94acba70571b158f
staticWagomU <to6wa17@gmail.com>
2024-06-06 22:19:46 +0900

    update refactor

--- rgb_convertion.rb
+++ rgb_convertion.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
 def to_hex(red, green, blue)
-  hex = '#'
-  [red, green, blue].each do |n|
-    hex += n.to_s(16).rjust(2, '0')
+  # ['B', 'C', 'D'].sum('A') #=> 'ABCD'
+  [red, green, blue].sum('#') do |n|
+    n.to_s(16).rjust(2, '0')
   end
-  hex
 end
 
