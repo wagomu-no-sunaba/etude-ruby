20a3e49f4260f79bf3e5bf54b63ec3cc6a5622c4
staticWagomU <to6wa17@gmail.com>
2024-06-06 21:52:45 +0900

    fizzbuzz!!!

---
+++ fizzbuzz.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+def fizz_buzz(num)
+  if num % 15 == 0
+    'Fizz Buzz'
+  elsif num % 3 == 0
+    'Fizz'
+  elsif num % 5 == 0
+    'Buzz'
+  else
+    num.to_s
+  end
+end
