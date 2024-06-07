a02ccb12ffb1f09b5bf1850766343d6ea1edefa5
staticWagomU <to6wa17@gmail.com>
2024-06-07 22:19:36 +0900

    update

--- array_for_etc.rb
+++ array_for_etc.rb
@@ -93,3 +93,44 @@
 # a = (1..5).to_a
 # a = [*1..5] # これでも同じ
 # pp a #=> [1, 2, 3, 4, 5]
+
+# a = (1..5).to_a
+# pp a[1..3] #=> [2, 3, 4]
+# pp a.values_at(0, 2, 4) #=> [1, 3, 5]
+# pp a[2, 3] #=> [3, 4, 5]
+# pp a[-1] #=> 5
+# pp a[-2, 2] #=> [4, 5]
+# pp a.last #=> 5
+# pp a.last(2) #=> [4, 5]
+# pp a.first #=> 1
+# pp a.first(2) #=> [1, 2]
+
+# a[-5] = 10
+# pp a #=> [10, 2, 3, 4, 5]
+# これはエラーになる
+# a[-6] = 0 # index -6 too small for array; minimum: -5 (IndexError)
+
+# a = (1..3).to_a
+# b = (3..5).to_a
+# require 'set'
+# a = Set[1, 2, 3]
+# b = Set[3, 4, 5]
+# pp a | b #=> [1, 2, 3, 4, 5]
+# pp a - b #=> [1, 2]
+# pp a & b #=> [3]
+
+# a,*b = (1..5).to_a
+# pp a #=> 1
+# pp b #=> [2, 3, 4, 5]
+
+# a = []
+# b = [2, 3]
+# a.push(1)
+# pp a #=> [1]
+# a.push(*b)
+# pp a #=> [1, 2, 3]
+
+a = Array.new(5, 'Hello')
+pp a #=> [1, 1, 1, 1, 1]
+a[1] = 'World'
+pp a
