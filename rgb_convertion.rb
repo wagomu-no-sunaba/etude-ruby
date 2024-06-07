24992ce769d32fbc1b4e5af1d74418d1ece1f31d
staticWagomU <to6wa17@gmail.com>
2024-06-07 09:39:13 +0900

    feat: to_ints実装

--- rgb_convertion.rb
+++ rgb_convertion.rb
@@ -7,3 +7,14 @@ def to_hex(red, green, blue)
   end
 end
 
+def to_ints(hex)
+  r = hex[1..2]
+  g = hex[3..4]
+  b = hex[5..6]
+
+  ints = []
+  [r, g, b].each do |s|
+    ints << s.hex
+  end
+  ints
+end
