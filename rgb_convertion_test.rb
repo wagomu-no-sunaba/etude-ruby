087ad243212f3f295802a84c1852db1d48993ff4
staticWagomU <to6wa17@gmail.com>
2024-06-07 09:36:58 +0900

    rename

--- rgb_convertion_test.rb
+++ rgb_convertion_test.rb
@@ -5,7 +5,7 @@ require_relative 'rgb_convertion'
 
 # TestRGB class
 class TestRGB < Minitest::Test
-  def test_rgb
+  def test_to_hex
     assert_equal '#000000', to_hex(0, 0, 0)
     assert_equal '#ffffff', to_hex(255, 255, 255)
     assert_equal '#043c78', to_hex(4, 60, 120)
